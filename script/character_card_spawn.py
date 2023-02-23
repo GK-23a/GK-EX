813844b59ae19e608990071838cdcea8abf2c052
GoldSheep3 <gold_sheep_3@163.com>
2023-02-23 11:31:29 +0800

    技能生成完成

--- script/character_card_spawn.py
+++ script/character_card_spawn.py
@@ -12,7 +12,7 @@ font['name'] =  ImageFont.truetype('font/SIMLI.TTF',              encoding='UTF-
 font['topic'] = ImageFont.truetype('font/SIMLI.TTF',              encoding='UTF-8', size=120) # 技能标题
 font['category'] = ImageFont.truetype('font/MiSans-Semibold.ttf', encoding='UTF-8', size=72) # 技能类型
 font['text'] =  ImageFont.truetype('font/MiSans-Regular.ttf',     encoding='UTF-8', size=72) # 技能内容
-font['sign'] =  ImageFont.truetype('font/MiSans-Light.ttf',       encoding='UTF-8', size=72) # 卡底标记
+font['sign'] =  ImageFont.truetype('font/MiSans-Light.ttf',       encoding='UTF-8', size=56) # 卡底标记
 
 # 颜色预定义
 element_color = {}
@@ -78,57 +78,60 @@ with open('json/characters.json', encoding='UTF-8') as jsonfile:
 # --------------------
 
 # DEBUG
-ch_id = 'xiao'
+ch_id = 'xingqiu'
 # for ch_id in character_data:
     
 # 图层顺序：空白卡底、角色立绘、技能说明、元素外框、神之眼（底座和图案）、称号、名称、血条、初始护盾   
 
 # 空白卡底
-cardbg = Image.new('RGBA', (2480,3480), (255,255,255,0))
+cardimg = Image.new('RGBA', (2480,3480), (255,255,255,0))
+
 # 角色立绘
 with Image.open('img/character/' + ch_id + '.png') as character_image:
-    cardbg.paste(character_image, (380,120))
+    cardimg.paste(character_image, (380,120))
 
 # 技能说明
-
-skillbg = Image.new('RGBA', (2000,3240), (253,253,253,138))
-
-
+skillimg = Image.new('RGBA', (2000,3240), (253,253,253,138))
 # 技能文本
 # INFO：未包含【技能类别粗体】【花色显示优化】
 height = 50
-number = 0
-color = element_color[character_data[ch_id]['element']]
-imgdraw(skillbg, (50, height), character_data[ch_id]['skills'][number]['name'], color[0], 'topic', 4, color[1])
 length = 45
-skilltext = ''
-skilltext_origin = character_data[ch_id]['skills'][number]['description']
-while True:
+color = element_color[character_data[ch_id]['element']]
+for skill_data in character_data[ch_id]['skills']:
+    fill_color = color[0]
+    if not skill_data['origin']:
+        fill_color = 'black'
+    imgdraw(skillimg, (50, height), skill_data['name'], fill_color, 'topic', 7, color[1])
+    skilltext = ''
+    skilltext_origin = skill_data['description']
     while True:
-        textline = skilltext_origin[:length]
-        linelen = ImageDraw.Draw(skillbg).multiline_textbbox((50, height+86), textline, font['text'], align='left', direction='ltr', language='zh-Hans')
-        if linelen[2] > 1940:
-            length -= 1
-        else:
-            # 中文标点修正
-            if textline[-1:] in left_punctuation:
-                length -= 1
-            elif linelen[length+1:length+1] in right_punctuation:
-                length += 1
+        while True:
             textline = skilltext_origin[:length]
+            linelen = ImageDraw.Draw(skillimg).multiline_textbbox((50, height+85), textline, font['text'], align='left', direction='ltr', language='zh-Hans')
+            if linelen[2] > 1940:
+                length -= 1
+            else:
+                # 中文标点修正
+                if textline[-1:] in left_punctuation:
+                    length -= 1
+                elif linelen[length+1:length+1] in right_punctuation:
+                    length += 1
+                textline = skilltext_origin[:length]
+                break
+        skilltext += textline + '\n'
+        skilltext_origin = skilltext_origin[length:]
+        if skilltext[-2:] == '\n\n':
+            skilltext = skilltext[:-2]
             break
-    skilltext += textline + '\n'
-    skilltext_origin = skilltext_origin[length:]
-    if skilltext[-2:] == '\n\n':
-        skilltext = skilltext[:-2]
-        break
-    else:
-        length = 45
-imgdraw(skillbg, (50, height+85), skilltext, 'black')
-linelen = ImageDraw.Draw(skillbg).multiline_textbbox((50, height+86), textline, font['text'], align='left', direction='ltr', language='zh-Hans')
+        else:
+            length = 45
+    imgdraw(skillimg, (50, height+85), skilltext, 'black')
+    height = ImageDraw.Draw(skillimg).multiline_textbbox((50, height+85), skilltext, font['text'], align='left', direction='ltr', language='zh-Hans')[3] + 35
+imgdraw(skillimg, (50, height+10), 'GenshinKill 2023ver. Alpha | Designer: ' + character_data[ch_id]['developer'] + ' , Artist: miHoYo', 'black', 'sign')
+skillimg = skillimg.crop((0,0,2000,height+100))
 # DEBUG
-skillbg.show()
-# skillbg.show()
+skillimg.show()
+# skillimg.show()
 
 
 
@@ -154,5 +157,5 @@ skillbg.show()
 # 技能层大小2000宽
 
 # DEBUG
-# cardbg.show()
+# cardimg.show()
 
