0e01985745a592513baacc7ead24f51a4dbc519b
GoldSheep3 <gold_sheep_3@163.com>
2023-08-15 14:50:38 +0800

    Update CardSpawn.py

--- script/Genshin/CardSpawn.py
+++ script/Genshin/CardSpawn.py
@@ -1,22 +1,22 @@
+from CardBuild import *
+
 import json
 import os
-
-from .CardBuild import *
 
 # build_a4_print: bool | 是否生成A4尺寸的打印版图片
 build_a4_print = False
 
 # all_character: bool | 是否生成全部角色卡；若为True，忽略character_list
-all_character = True
+all_character = False
 
 # character_list: list | 生成角色卡的列表
-character_list = ['']
+character_list = ['alhaitham']
 
 # 读取json
 with open(os.path.join('json', 'genshin-impact.json'), encoding='UTF-8') as file:
     file_data = json.loads(file.read())
     character_datas = file_data['character_data']
-    card_versions = file_data['versions']
+    card_versions = file_data['character_data_versions']
 
 if not os.path.exists(os.path.join('output', 'character_card')):
     os.makedirs(os.path.join('output', 'character_card'))
@@ -30,7 +30,7 @@ else:
         if data['id'] in character_list:
             datas.append(data)
 for data in datas:
-    if data['design_info']:
+    if data['design_state']:
         ch_img = genshin_character_card(data, card_versions)
         ch_img.save(os.path.join('output', 'character_card', data['id'] + '.png'))
         character_list.append(ch_img)
