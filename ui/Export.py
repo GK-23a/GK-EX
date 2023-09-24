9df900f8094782f6f84cc264c8d161223706d522
GoldSheep3 <gold_sheep_3@163.com>
2023-09-24 15:19:26 +0800

    导出 GUI解决

--- ui/Export.py
+++ ui/Export.py
@@ -1,10 +1,11 @@
 import json
 import os
+import sys
 
 from PySide6.QtCore import QRect
 from PySide6.QtGui import QFontDatabase, QFont
 from PySide6.QtWidgets import (QWidget, QApplication, QScrollArea, QLabel, QCheckBox, QPushButton, QRadioButton,
-                               QGroupBox)
+                               QGroupBox, QComboBox, QProgressBar)
 
 
 class CustomCheckBox(QCheckBox):
@@ -102,18 +103,19 @@ class EditWindow(QWidget):
         self.character_board.setGeometry(QRect(0, 0, 300, board_height))
 
         # 全选、全不选、选中设计完成的角色
-        edit_id = QPushButton(self)
-        edit_id.setGeometry(QRect(360, 25, 52, 22))
-        edit_id.setText('全选')
-        edit_id.clicked.connect(self.select_all)
-        edit_id = QPushButton(self)
-        edit_id.setGeometry(QRect(360+60, 25, 64, 22))
-        edit_id.setText('全不选')
-        edit_id.clicked.connect(self.deselect_all)
-        edit_id = QPushButton(self)
-        edit_id.setGeometry(QRect(360+132, 25, 140, 22))
-        edit_id.setText('选中设计完成的角色')
-        edit_id.clicked.connect(self.select_designed)
+        self.select_pushbutton = dict()
+        self.select_pushbutton['select_all'] = QPushButton(self)
+        self.select_pushbutton['select_all'].setGeometry(QRect(360, 25, 52, 22))
+        self.select_pushbutton['select_all'].setText('全选')
+        self.select_pushbutton['select_all'].clicked.connect(self.select_all)
+        self.select_pushbutton['deselect_all'] = QPushButton(self)
+        self.select_pushbutton['deselect_all'].setGeometry(QRect(360+60, 25, 64, 22))
+        self.select_pushbutton['deselect_all'].setText('全不选')
+        self.select_pushbutton['deselect_all'].clicked.connect(self.deselect_all)
+        self.select_pushbutton['select_designed'] = QPushButton(self)
+        self.select_pushbutton['select_designed'].setGeometry(QRect(360+132, 25, 140, 22))
+        self.select_pushbutton['select_designed'].setText('选中设计完成的角色')
+        self.select_pushbutton['select_designed'].clicked.connect(self.select_designed)
 
         # 数显区域
         show_all = QLabel(self)
@@ -138,20 +140,73 @@ class EditWindow(QWidget):
 
         # 导出
         image_box = QGroupBox(self)
-        image_box.setGeometry(QRect(360, 165, 120, 350))
-        image_box.setTitle('导出')
+        image_box.setGeometry(QRect(360, 155, 274, 250))
         show_export_for = QLabel(self)
         show_export_for.setText('导出为：')
-        show_export_for.setGeometry(QRect(360, 165, 60, 16))
-        export_markdown = QRadioButton(self)
-        export_markdown.setText('Markdown(*.md)')
-        export_markdown.setGeometry(QRect(360+60, 165, 200, 16))
-        export_plaintext = QRadioButton(self)
-        export_plaintext.setText('纯文本(*.txt)')
-        export_plaintext.setGeometry(QRect(360+60, 165+24, 200, 16))
-        export_image = QRadioButton(self)
-        export_image.setText('图片(*.png)')
-        export_image.setGeometry(QRect(360+60, 165+48, 200, 16))
+        show_export_for.setGeometry(QRect(370, 165, 60, 16))
+        self.export_button = dict()
+        self.export_button['image'] = QRadioButton(self)
+        self.export_button['image'].setText('图片(*.png)')
+        self.export_button['image'].setGeometry(QRect(370+60, 165, 200, 16))
+        self.export_button['image'].clicked.connect(self.update_export_location)
+        self.export_button['markdown'] = QRadioButton(self)
+        self.export_button['markdown'].setText('Markdown(*.md)')
+        self.export_button['markdown'].setGeometry(QRect(370+60, 165+24, 200, 16))
+        self.export_button['markdown'].clicked.connect(self.update_export_location)
+        self.export_button['plaintext'] = QRadioButton(self)
+        self.export_button['plaintext'].setText('纯文本(*.txt)')
+        self.export_button['plaintext'].setGeometry(QRect(370+60, 165+48, 200, 16))
+        self.export_button['plaintext'].clicked.connect(self.update_export_location)
+        self.export_button['image'].setChecked(True)
+        self.export_category = 'image'
+
+        self.image_export_way = QComboBox(self)
+        self.image_export_way.addItem('高清单图')
+        self.image_export_way.addItem('低清单图')
+        self.image_export_way.addItem('高清打印图')
+        self.image_export_way.setGeometry(QRect(370+60+100, 163, 92, 19))
+
+        export_open_folder = QPushButton(self)
+        export_open_folder.setText('打开目标文件夹')
+        export_open_folder.setGeometry(QRect(550-110, 238, 104, 22))
+        export_open_folder.clicked.connect(lambda: self.open_folder())
+        self.export_run = QPushButton(self)
+        self.export_run.setText('确认导出')
+        self.export_run.setGeometry(QRect(550, 238, 72, 22))
+        self.export_run.clicked.connect(self.export)
+
+        self.export_bar = [dict(), dict()]
+        self.export_bar[0]['bar'] = QProgressBar(self)
+        self.export_bar[0]['bar'].setGeometry(QRect(370, 325, 294, 20))
+        self.export_bar[0]['text'] = QLabel(self)
+        self.export_bar[0]['text'].setGeometry(QRect(370, 325-20, 294, 16))
+        self.export_bar[0]['text'].setText('')
+        self.export_bar[1]['bar'] = QProgressBar(self)
+        self.export_bar[1]['bar'].setGeometry(QRect(370, 370, 294, 20))
+        self.export_bar[1]['text'] = QLabel(self)
+        self.export_bar[1]['text'].setGeometry(QRect(370, 370-20, 294, 16))
+        self.export_bar[1]['text'].setText('')
+
+    def export(self):
+        self.select_pushbutton['select_all'].setDisabled(True)
+        self.select_pushbutton['deselect_all'].setDisabled(True)
+        self.select_pushbutton['select_designed'].setDisabled(True)
+        self.export_run.setDisabled(True)
+        if self.export_category == 'image':
+            self.image_export_way.currentIndex()
+        else:
+            file_category = self.export_category
+            # if self.export_category == 'md':
+            # if self.export_category == 'plaintext':
+
+    def update_export_location(self):
+        sender = self.sender()
+        if sender == self.export_button['image']:
+            self.export_category = 'image'
+        elif sender == self.export_button['markdown']:
+            self.export_category = 'md'
+        elif sender == self.export_button['plaintext']:
+            self.export_category = 'txt'
 
     def update_count_label(self):
         checked_count = sum([v.isChecked() for v in self.ndl.values()])
@@ -174,6 +229,12 @@ class EditWindow(QWidget):
             else:
                 v.setChecked(True)
         self.update_count_label()
+
+    def open_folder(self):
+        folder_dir = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), 'output', self.export_category)
+        if not os.path.exists(folder_dir):
+            os.makedirs(folder_dir)
+        os.startfile(folder_dir)
 
 
 if __name__ == "__main__":
